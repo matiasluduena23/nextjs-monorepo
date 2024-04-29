00d2ffd78ba279bd44b9fdeb28d1d9af4360783d
Matias Ludueña <matiasluduena23@gmail.com>
2024-04-29 20:25:35 -0300

    boilerplate ready

--- apps/e-commerce/tailwind.config.ts
+++ apps/e-commerce/tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {},
+		colors: {
+			clOrange: 'hsl(26, 100%, 55%)',
+			clPaleOrange: 'hsl(25, 100%, 94%)',
+			clDarkBlue: 'hsl(220, 13%, 13%)',
+			clDarkGreyblue: 'hsl(219, 9%, 45%)',
+			clGrayishblue: 'hsl(220, 14%, 75%)',
+			clLightgrayishblue: 'hsl(223, 64%, 98%)',
+		},
+	},
+	plugins: [],
 };
 export default config;
